2d56d5256e779f2217df2470e318b92e2cdd8710
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2018-05-30 08:00:31 +0200

    autoadd namespace if it is not configured

--- funcs.sh
+++ funcs.sh
@@ -1,19 +1,42 @@
 export KUBECONFIG="none"
 
 kcs() {
-  CLUSTER=$1
-  NAMESPACE=${2:-default}
+  local CLUSTER=$1
+  local NAMESPACE=${2:-default}
   [ -z "$CLUSTER" ] && echo "cluster name needed" && return 1
-  FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
-  [ ! -e "$FILE" ] && echo "unknown $CLUSTER($NAMESPACE)" && return 1
+  local FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
+  if [ ! -e "$FILE" ]; then
+    local DEFAULT="$HOME/.kube/config|$CLUSTER|default"
+    if [ ! -e "$DEFAULT" ]; then
+      echo "Unknown cluster $CLUSTER"
+      return 1
+    fi
+    echo "first time $NAMESPACE usage. Adding it"
+    _add_namespace "${CLUSTER}" "${NAMESPACE}"
+  fi
   export KUBECONFIG="$FILE"
 }
 
 kns() {
-  NAMESPACE=$1
+  local NAMESPACE=$1
   [ -z "$NAMESPACE" ] && echo "namespace name needed" && return 1
-  CLUSTER=`echo $KUBECONFIG|cut -f2 -d'|'`
-  FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
-  [ ! -e "$FILE" ] && echo "unknown $CLUSTER($NAMESPACE)" && return 1
+  local CLUSTER=`echo $KUBECONFIG|cut -f2 -d'|'`
+  local FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
+  if [ ! -e "$FILE" ]; then
+    echo "first time $NAMESPACE usage. Adding it"
+    _add_namespace "${CLUSTER}" "${NAMESPACE}"
+  fi
   export KUBECONFIG="$FILE"
+}
+
+_add_namespace() {
+  local CLUSTER=$1
+  local NAMESPACE=$2
+  [ -z "$CLUSTER" ] && return 1
+  [ -z "$NAMESPACE" ] && return 1
+  kcc add-namespace "${CLUSTER}" "${NAMESPACE}"
+  if [ $? -ne 0 ]; then
+    echo "error adding namespace"
+    return 1
+  fi
 }
