fc43d6c9d2c061dc866946479a06e8d1ab594b5c
José Luis Ledesma Piqueras <jose@netlify.com>
2019-07-31 17:20:14 +0200

    update to get rid of python ^^

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 tools for managing multiple k8s clusters
+
+source funcs.sh in your .bashrc or .profile
+
+kubeconfig files expect to have config#clustername#namespace format, so when
+you need to configure a new cluster you should point KUBECONFIG there (yep,
+rudimentary ^^)
+
+autocomplete file may be added to your bash_completion.d
--- funcs.sh
+++ funcs.sh
@@ -1,12 +1,17 @@
 export KUBECONFIG="none"
+alias k=kubectl
 
 kcs() {
   local CLUSTER=$1
   local NAMESPACE=${2:-default}
   [ -z "$CLUSTER" ] && echo "cluster name needed" && return 1
-  local FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
+  if [ "$CLUSTER" = "none" ]; then
+    unset KUBECONFIG
+    return
+  fi
+  local FILE="$HOME/.kube/config#$CLUSTER#$NAMESPACE"
   if [ ! -e "$FILE" ]; then
-    local DEFAULT="$HOME/.kube/config|$CLUSTER|default"
+    local DEFAULT="$HOME/.kube/config#$CLUSTER#default"
     if [ ! -e "$DEFAULT" ]; then
       echo "Unknown cluster $CLUSTER"
       return 1
@@ -14,18 +19,21 @@ kcs() {
     echo "first time $NAMESPACE usage. Adding it"
     _add_namespace "${CLUSTER}" "${NAMESPACE}"
   fi
+  export K8SCLUSTER="$CLUSTER"
+  export K8SNAMESPACE="$NAMESPACE"
   export KUBECONFIG="$FILE"
 }
 
 kns() {
   local NAMESPACE=$1
   [ -z "$NAMESPACE" ] && echo "namespace name needed" && return 1
-  local CLUSTER=`echo $KUBECONFIG|cut -f2 -d'|'`
-  local FILE="$HOME/.kube/config|$CLUSTER|$NAMESPACE"
+  local CLUSTER=`echo $KUBECONFIG|cut -f2 -d'#'`
+  local FILE="$HOME/.kube/config#$CLUSTER#$NAMESPACE"
   if [ ! -e "$FILE" ]; then
     echo "first time $NAMESPACE usage. Adding it"
     _add_namespace "${CLUSTER}" "${NAMESPACE}"
   fi
+  export K8SNAMESPACE="$NAMESPACE"
   export KUBECONFIG="$FILE"
 }
 
@@ -34,7 +42,11 @@ _add_namespace() {
   local NAMESPACE=$2
   [ -z "$CLUSTER" ] && return 1
   [ -z "$NAMESPACE" ] && return 1
-  kcc add-namespace "${CLUSTER}" "${NAMESPACE}"
+  local DEFAULT="$HOME/.kube/config#$CLUSTER#default"
+  local FILE="$HOME/.kube/config#$CLUSTER#$NAMESPACE"
+  cp "${DEFAULT}" "${FILE}"
+  export KUBECONFIG="${FILE}"
+  kubectl config set-context $CLUSTER --namespace=$NAMESPACE
   if [ $? -ne 0 ]; then
     echo "error adding namespace"
     return 1
